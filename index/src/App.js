21a13ea4be3aa360186a3b3cd1e2b280f1c4ee9c
jc2729 <jc2729@cornell.edu>
2017-06-14 22:08:20 -0700

    Removed irrelevant files & set up very basic framework for Feed/Sidebar/Article

--- index/src/App.js
+++ index/src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import ButtonToolbar from 'react-bootstrap';
 
-class App extends Component {
+
+class Article extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="article">
+        <h1> this represents an article </h1>
+        <p> read more about the article at this LINK </p>
+
       </div>
     );
   }
 }
 
-export default App;
+
+class Sidebar extends React.Component {
+
+
+  render() {
+    return (
+      <div className="sidebar">
+        <h1> Hello, [name] </h1>
+        <p> [img] </p>
+      
+      </div>
+    );
+  }
+
+}
+
+class Feed extends React.Component {
+
+  render() {
+   
+    return (
+      <div>
+        <Sidebar />
+        <Article />
+      </div>
+    );
+  }
+
+  
+}
+export default Feed;
--- index/src/index.js
+++ index/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import Feed from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+  <Feed />,
+  document.getElementById('root'),
+);
