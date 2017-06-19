a98fcf34ee46d879eb822a62a0429d485f831ee9
Andy Zhang <jz359@cornell.edu>
2017-06-19 16:25:04 -0700

    added skeleton main backend class; OO-ified the skipgram model for future interfacing with main.py; still need to connect main.py to crawler

---
+++ ml/main.py
@@ -0,0 +1,23 @@
+import os
+import numpy as np
+import zipfile
+import collections
+import tensorflow as tf
+from six.moves import urllib
+from skipgram import SkipGram
+#from webcrawler import WebCrawler
+
+'''interacts with database and webcrawler modules (not sure how that will work just yet)
+also main communication point w/ frontend'''
+class BackendHandler(object):
+	def __init__(self):
+		#initialize a crawler
+		#self.crawler = WebCrawler()
+
+	def getUserModel(userid):
+		#TODO: grab model data from database w/ userid
+
+	def makeUserModel(userid):
+		#TODO: train model with a zip of a bunch of articles pulled
+
+	def getLinks():
