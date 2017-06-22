76e29e1938730a0b42dc24b8b9a3d9d825044c37
Andy Zhang <jz359@cornell.edu>
2017-06-22 14:36:24 -0700

    renamed ml folder lel

--- backend/webcrawler.py
+++ backend/webcrawler.py
@@ -68,5 +68,5 @@ crawler = WebCrawler()
 keywords = ['global', 'warming']
 links = crawler.crawl('http://www.bing.com/search?q=global+warming&go=Submit&qs=bs&form=QBLH', keywords)
 #print links
-paragraphs = crawler.grabContent(links[1])
+paragraphs = crawler.grabContent(links[3])
 print paragraphs
