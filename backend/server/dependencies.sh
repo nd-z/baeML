9c1046f3ea480e042603fc3405425317280455f3
jc2729 <jc2729@cornell.edu>
2017-07-02 14:16:16 -0700

    Added postgres check to dependencies script

--- backend/server/dependencies.sh
+++ backend/server/dependencies.sh
@@ -3,6 +3,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLACK='\033[0;30m'
+
 #Make sure Python 2.7 is installed.
 function python_check() {
 	python_version=$(python --version 2>&1)
@@ -12,9 +13,21 @@ function python_check() {
 		echo -e "${RED}Please install Python 2.7${BLACK}"
 		exit 1
 	fi
+
+}
+
+function postgres_check() {
+	postgres_installed=$(which psql)
+	if [[ "$postgres_installed" == "" ]]; then
+		echo -e "${RED}Please install Postgres${BLACK}"
+		exit 1
+	else
+		echo -e "${GREEN}Postgres Check OK ${BLACK}"
+	fi
 }
 
 python_check
+postgres_check
 
 echo -e "${GREEN}Installing Django Dependencies...${BLACK}"
 pip install Django
