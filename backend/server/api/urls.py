20de1a6e5060fb0017941b840165e83d4d2c4cf8
jc2729 <jc2729@cornell.edu>
2017-07-07 22:38:30 -0700

    Debugging cpickle import error

--- backend/server/api/urls.py
+++ backend/server/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^login/', views.UsersView.as_view()),
     url(r'^init/', views.UsersView.as_view()),
     url(r'^test/', api_handlers.hello, name="hello"),
-    url(r'^users/(?P<user_id>\d+)/keywords', main_handler.post)
+    url(r'^users/(?P<user_id>\d+)/keywords', main_handler.post),
     url(r'^users/(?P<user_id>\d+)/articles/next', main_handler.get)
 ]
